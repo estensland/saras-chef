6ad4d59ea7b0506956360688d8242e9576ba7b40
Sara Trice <sara.trice@wowzers.com>
2012-12-06 10:07:30 -0600

    removing sphinx

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -5,7 +5,7 @@
 #end
 
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
-require_recipe "sphinx"
+# require_recipe "sphinx"
 
 # turn on whenever (crontab scheduling gem)
 require_recipe "whenever"
@@ -113,7 +113,7 @@ require_recipe "mongodb"
   # postgresql9_isn "dbname"
   # postgresql9_lo "dbname"
   # postgresql9_ltree "dbname"
-  # postgresql9_pg_trgm "dbname"  
+  # postgresql9_pg_trgm "dbname"
   # postgresql9_pgcrypto "dbname"
   # postgresql9_pgrowlocks "dbname"
   # postgresql9_postgis "dbname"
@@ -123,15 +123,15 @@ require_recipe "mongodb"
   # postgresql9_test_parser "dbname"
   # postgresql9_unaccent "dbname"
   # postgresql9_uuid_ossp "dbname"
-  
-  
+
+
   # 9.1 Extensions
-  # postgresql9_file_fdw "dbname" 
+  # postgresql9_file_fdw "dbname"
   # postgresql9_xml2 "dbname"
-  
+
   #Admin-Level Contribs
   # postgresql9_pg_buffercache "postgres"
   # postgresql9_pg_freespacemap "postgres"
   # postgresql9_pg_stat_statements "todo" - Not done
-  
+
 # end
