8c2149ae5ca15fd603ce96e58f38e35982309891
Sara Trice <sara.trice@wowzers.com>
2012-12-08 12:31:38 -0600

    removing default mongodb recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -41,7 +41,7 @@ require_recipe "whenever"
 
 #uncomment to run the exim::auth recipe
 #require_recipe "exim::auth"
-require_recipe "mongodb"
+# require_recipe "mongodb"
 
 #uncomment to run the resque recipe
 # require_recipe "resque"
