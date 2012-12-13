09318218c99ed0e26c1f0e0e1823997acd77994b
Sam Granieri <sam@wowzers.com>
2012-12-13 15:26:34 -0600

    fixing a bug?

--- cookbooks/sidekiq/recipes/restart.rb
+++ cookbooks/sidekiq/recipes/restart.rb
@@ -1,16 +1,18 @@
 
 if sidekiq_instance?
 
-  execute "ensure-sidekiq-is-setup-with-monit" do 
-    command %Q{ 
-      monit reload 
-    } 
-  end
+  node[:applications].each do |app, data|
+    execute "ensure-sidekiq-is-setup-with-monit" do
+      command %Q{
+      monit reload
+      }
+    end
 
-  execute "restart-sidekiq" do 
-    command %Q{ 
-      echo "sleep 20 && monit -g #{app}_sidekiq restart all" | at now 
-    }
+    execute "restart-sidekiq" do
+      command %Q{
+      echo "sleep 20 && monit -g #{app}_sidekiq restart all" | at now
+      }
+    end
   end
 
 end
