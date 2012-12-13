20976da45c16e218b672946d6af9c1c38be32947
Sam Granieri <sam@wowzers.com>
2012-12-13 15:18:32 -0600

    bringing in sidekiq

--- cookbooks/whenever/recipes/default.rb
+++ cookbooks/whenever/recipes/default.rb
@@ -12,16 +12,18 @@ appname = "wowzDashboard"
 
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
 
+  if node[:environment][:framework_env] == 'production'
+    local_user = node[:users].first
+    execute "whenever" do
+      cwd "/data/#{appname}/current"
+      user local_user[:username]
+      command "bundle exec whenever --update-crontab '#{appname}_#{node[:environment][:framework_env]}'"
+      action :run
+    end
+
+    ey_cloud_report "whenever" do
+      message "whenever recipe complete"
+    end
+  end
   # be sure to replace "app_name" with the name of your application.
-  local_user = node[:users].first
-  execute "whenever" do
-    cwd "/data/#{appname}/current"
-    user local_user[:username]
-    command "bundle exec whenever --update-crontab '#{appname}_#{node[:environment][:framework_env]}'"
-    action :run
-  end
-  
-  ey_cloud_report "whenever" do
-    message "whenever recipe complete"
-  end
 end
