6c3b3f2af1a1b687a755b10b6deb71958f204cd4
Sam Granieri <sam@wowzers.com>
2012-12-21 17:06:52 -0600

    shared sidekiq config

--- cookbooks/sidekiq/recipes/configure.rb
+++ cookbooks/sidekiq/recipes/configure.rb
@@ -23,7 +23,16 @@ if sidekiq_instance?
       mode 0755
       source "sidekiq.erb"
     end
-
+    template "/data/#{app}/shared/config/sidekiq.rb" do
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0644
+      source "sidekiq.rb.erb"
+      variables({
+        namespace: "'#{app}:#{node[:environment][:framework_env]}'",
+        url: "'redis://#{node['db_host']}:6379'"
+      })
+    end
     worker_count.times do |count|
       template "/data/#{app}/shared/config/sidekiq_#{count}.yml" do
         owner node[:owner_name]
