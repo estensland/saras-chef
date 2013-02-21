64fd15e8c0c9c8df1c4b7b269d0760a112bf64ac
Sara Trice <sara.trice@wowzers.com>
2013-02-21 17:47:56 -0600

    attempting sidekiq recipe update

--- cookbooks/sidekiq/recipes/configure.rb
+++ cookbooks/sidekiq/recipes/configure.rb
@@ -1,60 +1,37 @@
 if sidekiq_instance?
-
   # for now
   worker_count = 1
 
-  node[:applications].each do |app, data|
-    template "/etc/monit.d/sidekiq_#{app}.monitrc" do
+  template "/engineyard/bin/sidekiq" do
+    owner 'root'
+    group 'root'
+    mode 0755
+    source "sidekiq.erb"
+  end
+
+  node[:applications].each do |app_name, data|
+    template "/etc/monit.d/sidekiq_#{app_name}.monitrc" do
       owner 'root'
       group 'root'
       mode 0644
       source "monitrc.conf.erb"
       variables({
         :num_workers => worker_count,
-        :app_name => app,
-        :rails_env => node[:environment][:framework_env],
-        :user => node[:owner_name]
+        :app_name => app_name,
+        :rails_env => node[:environment][:framework_env]
       })
     end
 
-    template "/engineyard/bin/sidekiq" do
-      owner 'root'
-      group 'root'
-      mode 0755
-      source "sidekiq.erb"
-    end
-    template "/data/#{app}/shared/config/sidekiq.rb" do
-      owner node[:owner_name]
-      group node[:owner_name]
-      mode 0644
-      source "sidekiq.rb.erb"
-      variables({
-        :namespace=> "#{app}:#{node[:environment][:framework_env]}",
-        :url=> "redis://#{node['db_host']}:6379"
-      })
-    end
-    execute "remove current sidekiq symlink" do
-      command "rm -f /data/#{app}/current/config/initializers/sidekiq.rb"
-    end
-    execute "manually symlink" do
-      command "ln -nfs /data/#{app}/shared/config/sidekiq.rb /data/#{app}/current/config/initializers/sidekiq.rb"
-      user node[:owner_name]
-      group node[:owner_name]
-    end
     worker_count.times do |count|
-      template "/data/#{app}/shared/config/sidekiq_#{count}.yml" do
+      template "/data/#{app_name}/shared/config/sidekiq_#{count}.yml" do
         owner node[:owner_name]
         group node[:owner_name]
         mode 0644
         source "sidekiq.yml.erb"
         variables({
-          :require => "/data/#{app}/current",
-          :verbose => false,
-          :concurrency => 30,
-          :queues => { 'default' => 20, 'mailer' => 10 }
+          :require => "/data/#{app_name}/current"
         })
       end
     end
-
   end
 end
--- cookbooks/sidekiq/recipes/restart.rb
+++ cookbooks/sidekiq/recipes/restart.rb
@@ -1,17 +1,13 @@
 if sidekiq_instance?
-
   execute "ensure-sidekiq-is-setup-with-monit" do
-    command %Q{
-      monit reload
-    }
+    command "monit reload"
   end
 
-  node[:applications].each do |app, data|
+  node[:applications].each do |app_name, data|
     execute "restart-sidekiq" do
       command %Q{
-        echo "sleep 50 && monit -g #{app}_sidekiq restart all" | at now
+        echo "sleep 20 && monit -g #{app_name}_sidekiq restart all" | at now
       }
     end
   end
-
 end
