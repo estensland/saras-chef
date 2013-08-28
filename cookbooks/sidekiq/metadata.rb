b337d53a0be64af04bf0ba8e29146f97f2c059dc
Sara Trice <sara.trice@wowzers.com>
2013-08-28 15:35:03 -0500

    more updates

--- cookbooks/sidekiq/metadata.rb
+++
@@ -1,7 +0,0 @@
-name              "sidekiq"
-description       "Sidekiq recipe for EY Cloud"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-maintainer        "Dr Nic Williams"
-maintainer_email  "drnicwilliams@gmail.com"
-version           "0.2.0"
-# depends           "cronjobs"
