7e338d9cbc5349c2490c24d068c24cac044e3990
Sara Trice <sara.trice@wowzers.com>
2014-09-15 20:36:25 -0500

    bugfixing add header to conf

--- cookbooks/nginx_cors_fix/recipes/default.rb
+++ cookbooks/nginx_cors_fix/recipes/default.rb
@@ -7,16 +7,12 @@ if ['app_master', 'app', 'solo'].include?(node[:instance_role])
   node[:applications].each do |app, data|
 
     template "/tmp/location.block" do
-      source 'location.block'
+      source 'location.block.erb'
     end
 
     execute "add header to conf" do
-      command "sed -i '/error_log \/var\/log\/engineyard\/nginx\/#{app[:name]}.error.log notice;/r /tmp/location.block' /etc/nginx/servers/#{app[:name]}.conf"
-      not_if "grep -q Access-Control-Allow-Origin /etc/nginx/servers/#{app[:name]}.conf"
-    end
-    execute "add header to ssl conf" do
-      command "sed -i '/error_log \/var\/log\/engineyard\/nginx\/#{app[:name]}.error.log notice;/r /tmp/location.block' /etc/nginx/servers/#{app[:name]}.ssl.conf"
-      not_if "grep -q Access-Control-Allow-Origin /etc/nginx/servers/#{app[:name]}.ssl.conf"
+      command "sed -i '/error_log \\/var\\/log\\/engineyard\\/nginx\\/#{app}.ssl.error.log notice;/r /tmp/location.block' /etc/nginx/servers/#{app}.ssl.conf; sed -i '/error_log \\/var\\/log\\/engineyard\\/nginx\\/#{app}error.log notice;/r /tmp/location.block' /etc/nginx/servers/#{app}.conf"
+      not_if "grep -q Access-Control-Allow-Origin /etc/nginx/servers/#{app}.conf"
     end
 
   end
