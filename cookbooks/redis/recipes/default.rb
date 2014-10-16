7cd89468901815e7e1a82299702fd8467949d7d6
Eric <eric@wowzers.com>
2014-10-16 09:29:56 -0500

    changes for upgrading

--- cookbooks/redis/recipes/default.rb
+++ cookbooks/redis/recipes/default.rb
@@ -47,6 +47,13 @@ if ['util'].include?(node[:instance_role])
       })
     end
 
+    # redis-server is in /usr/bin on stable-v2, /usr/sbin for stable-v4
+    if Chef::VERSION[/^0.6/]
+      bin_path = "/usr/bin/redis-server"
+    else
+      bin_path = "/usr/sbin/redis-server"
+    end
+
     template "/data/monit.d/redis_util.monitrc" do
       owner 'root'
       group 'root'
@@ -58,6 +65,7 @@ if ['util'].include?(node[:instance_role])
         :pidfile => node[:redis][:pidfile],
         :logfile => node[:redis][:basename],
         :port => node[:redis][:bindport],
+        :bin_path
       })
     end
 
